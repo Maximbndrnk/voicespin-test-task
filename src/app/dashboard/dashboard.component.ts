1443b8f7dd91e965120d12c862a4ea9bc4d50c46
Maxim Bondarenko <maximbndrnk@gmail.com>
2019-08-06 16:55:54 +0300

    update markup, add some new logic

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -10,28 +10,57 @@ import {environment} from '../../environments/environment';
 })
 export class DashboardComponent implements OnInit, OnDestroy {
 
+  callStatuses = {
+    IDLE: 'IDLE',
+    IN_CALL: 'IN CALL',
+    PAUSED: 'PAUSED'
+  };
   options: GridsterConfig;
-  dashboard: Array<GridsterItem>;
+  agentWidget: GridsterItem = {cols: 2, rows: 1, y: 0, x: 0};
+  callWidget: GridsterItem = {cols: 2, rows: 2, y: 0, x: 2};
+
+  agents = {};
+  calls: [];
+
   private socketSubscription: any;
 
   constructor(private socketService: SocketService) {
-    this.socketSubscription = socketService.listen(environment.callCenterDataEmittedEventName).subscribe((data) => {
-      console.log('data', data);
+    this.connect();
+  }
+
+  connect() {
+    if (!this.socketSubscription) {
+      this.socketSubscription = this.socketService.listen(environment.callCenterDataEmittedEventName).subscribe((data: any) => {
+        console.log('data', data);
+        // this.agents = data.agents;
+        this.handleAgentsData(data.agents);
+        this.calls = data.calls;
+      });
+    }
+  }
+
+  handleAgentsData(agents: []) {
+    agents.forEach((a: any) => {
+      if ((a.name) in this.agents) {
+
+      } else {
+        this.agents[a.name] = a;
+      }
+
     });
+    console.log('this.agents', this.agents);
   }
 
-  itemChange(item, itemComponent) {
-    console.log('itemChanged', item, itemComponent);
+  disconnect() {
+    this.socketSubscription.unsubscribe();
+    this.socketSubscription = undefined;
   }
 
-  itemResize(item, itemComponent) {
-    console.log('itemResized', item, itemComponent);
-  }
 
   ngOnInit() {
     this.options = {
-      itemChangeCallback: this.itemChange,
-      itemResizeCallback: this.itemResize,
+      // itemChangeCallback: DashboardComponent.itemChange,
+      // itemResizeCallback: DashboardComponent.itemResize,
       draggable: {
         enabled: true
       },
@@ -40,27 +69,19 @@ export class DashboardComponent implements OnInit, OnDestroy {
         enabled: true
       }
     };
-
-    this.dashboard = [
-      {cols: 2, rows: 1, y: 0, x: 0},
-      {cols: 2, rows: 2, y: 0, x: 2}
-    ];
   }
 
-  changedOptions() {
-    this.options.api.optionsChanged();
-  }
-
-  removeItem(item) {
-    this.dashboard.splice(this.dashboard.indexOf(item), 1);
-  }
-
-  addItem() {
-    this.dashboard.push();
-  }
+  // static itemChange(item, itemComponent) {
+  //   console.log('itemChanged', item, itemComponent);
+  // }
+  //
+  // static itemResize(item, itemComponent) {
+  //   console.log('itemResized', item, itemComponent);
+  // }
 
   ngOnDestroy(): void {
     this.socketSubscription.unsubscribe();
+    this.socketSubscription = undefined;
   }
 
 }
